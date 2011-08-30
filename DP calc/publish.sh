e4c63077d128069240320ded098f4e38038c7324
william <william@william-pc.(none)>
2011-08-30 11:27:44 -0400

    Default update text here

--- DP calc/publish.sh
+++ DP calc/publish.sh
@@ -1,3 +1,4 @@
 rar a DP_Calc.rar ./stats.txt ./readMe.txt ./DP_Calc.jar ./stat_changer.jar
-scp DP_Calc.rar wjn1@unix.andrew.cmu.edu:www/DP_Calc.rar
-curl http://www.andrew.cmu.edu/cgi-bin/publish?FLAG=0&NAME=wjn1
+git add *
+git commit -m "Default update text here"
+git push
